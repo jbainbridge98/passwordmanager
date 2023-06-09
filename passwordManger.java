34bbfaafd9f2f6888e0109383a45660041fbda6f
Jackson Bainbridge <56710599+jbainbridge98@users.noreply.github.com>
2023-06-09 13:45:19 -0400

    Update passwordManger.java
    added the change and delete functions, both working as expected. Next step is to have the program write info to a file and read from said file, also add a login system

--- passwordManger.java
+++ passwordManger.java
@@ -26,6 +26,8 @@ class Info{
         this.user = user;
         this.pass = pass;
     }
+
+
 }
 
 public class passwordManager{
@@ -39,7 +41,6 @@ public class passwordManager{
             }
         }
         return -1;
-
     }
 
     public static void main(String args[]){
@@ -50,7 +51,7 @@ public class passwordManager{
 
         do{
             if(flag == 0){
-                System.out.println("Welcome to your password manager! Enter \"Find\" to find your password, enter \"Add\" to add a new password, or enter \"Change\" to change the information of a password");
+                System.out.println("Welcome to your password manager! Enter \"Find\" to find your password, enter \"Add\" to add a new password, enter \"Change\" to change the information of a password, or enter \"Delete\" to delete a password and the information related to it.");
             }else{
                 System.out.println("Enter \"Find\" to find your password, enter \"Add\" to add a new password, or enter \"Change\" to change the information of a password");
             }
@@ -82,9 +83,45 @@ public class passwordManager{
 
             }else if(choice.equalsIgnoreCase("Change")){
                 System.out.println("Please enter the website in which you would like to change the information for.");
-                //String changeInfo = input.nextLine();
+                String changeInfoWeb = input.nextLine();
+                int position = findWeb(passwords, changeInfoWeb);
+                System.out.println("Do you want to change the username or password?");
+                String userOrPass = input.nextLine();
+                
+                if(userOrPass.equalsIgnoreCase("username")){
+                    System.out.print("Enter the new username:");
+                    String newUser = input.nextLine();
+                    String oldWeb = passwords.get(position).getWeb();
+                    String oldPass = passwords.get(position).getPass();
+
+                    passwords.remove(position);
+                    Info updatedUser = new Info(oldWeb, newUser, oldPass);
+                    passwords.add(updatedUser);
+
+                }else if(userOrPass.equalsIgnoreCase("password")){
+                    System.out.print("Enter the new password:");
+                    String newPass = input.nextLine();
+                    String oldWeb = passwords.get(position).getWeb();
+                    String oldUser = passwords.get(position).getUser();
+
+                    passwords.remove(position);
+                    Info updatedUser = new Info(oldWeb, oldUser, newPass);
+                    passwords.add(updatedUser);
+                }else{
+                    System.out.println("ERROR: INVALID INPUT");
+                }
+                System.out.println("Current information; ");
+                System.out.println("Website: " + passwords.get(position).getWeb());
+                System.out.println("Username: " + passwords.get(position).getUser());
+                System.out.println("Password: " + passwords.get(position).getPass());
                 
 
+            }else if(choice.equalsIgnoreCase("Delete")){
+                System.out.println("Please enter the website in which you would like to delete the information for.");
+                String findInfo = input.nextLine();
+                int position = findWeb(passwords, findInfo);
+                passwords.remove(position);
+            
             }else{
                 System.out.println("Invalid Input!");
             }
