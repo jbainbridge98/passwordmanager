0504788ee9663cffbd8ae7199bcee8baa5f7668f
Jackson Bainbridge <56710599+jbainbridge98@users.noreply.github.com>
2023-06-17 16:14:45 -0400

    Update passwordManger.java
    Added a functionality for the code to understand when it is the first time starting up, also added a password functionality. The program also outputs all info to a file then on the next startup the program will retrieve all information from output.txt. May add an encryption functionality.

--- passwordManger.java
+++ passwordManger.java
@@ -1,5 +1,10 @@
 import java.util.*;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Paths;
 
 class Info{
 
@@ -43,30 +48,73 @@ public class passwordManager{
         return -1;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException{
         
-        int flag = 0;
+        int flag = 0; //0 = do not continue looping, 1 = continue looping
         Scanner input = new Scanner(System.in);
         List<Info> passwords = new ArrayList<>();
+        String masterPass = "";
+        /*beggining ideas of the password function. Idea is if output.txt already exists then this is not
+        the first time user is starting program. Set a flag for first time running, if it is indeed the
+        first time running program will prompt user to make a username and password for the password manager,
+        said password will be saved to the output.txt file.*/
 
+        if(Paths.get("C:\\Users\\baind\\OneDrive\\Desktop\\Java Projects\\output.txt").toFile().exists()){
+            System.out.println("Welcome back to your password manager! Please enter your password");
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            for(int i = 0; i < 5; i++){
+                String pwordCheck = input.nextLine();
+                masterPass = reader.readLine();
+                if(!pwordCheck.equalsIgnoreCase(masterPass)){
+                    System.out.println("WRONG PASSWORD, TRY AGAIN");
+                    if(i == 4){
+                        //maybe make option after 5 failed attempts to either shut down or delete password file
+                        System.out.println("EXCEEDED 5 ATTEMPTS, SHUTTING DOWN NOW");
+                        System.exit(0);
+                    }
+                }else{
+                    break;
+                }
+            }
+            String line = reader.readLine();
+            while(line != null){
+                String inpweb = line.substring(0, line.indexOf("!"));
+                String inpuser = line.substring(line.indexOf("!") + 1, line.indexOf("@"));
+                String inppass = line.substring(line.indexOf("@") + 1);
+                Info oldInfo = new Info(inpweb, inpuser, inppass);
+                passwords.add(oldInfo);
+                line = reader.readLine();
+            }
+            reader.close();
+        }else{
+            File opt = new File("C:\\Users\\baind\\OneDrive\\Desktop\\Java Projects\\output.txt");
+            opt.createNewFile();
+            System.out.print("Welcome to your password manager! Please enter a password to secure your saved passwords: ");
+            masterPass = input.nextLine();
+            System.out.println("WARNING: DO NOT FORGET THIS PASSWORD, IT IS REQUIRED TO ACCESS YOUR SAVED PASSWORDS!");
+        }
+        FileWriter writer = new FileWriter("output.txt");
         do{
             if(flag == 0){
-                System.out.println("Welcome to your password manager! Enter \"Find\" to find your password, enter \"Add\" to add a new password, enter \"Change\" to change the information of a password, or enter \"Delete\" to delete a password and the information related to it.");
+                System.out.println("Welcome to your password manager! Enter \"Find\" to find your password, enter \"Add\" to add a new password, enter \"Change\" to change the information of a password, enter \"Delete\" to delete a password and the information related to it, or enter \"Quit\" to exit.");
             }else{
                 System.out.println("Enter \"Find\" to find your password, enter \"Add\" to add a new password, or enter \"Change\" to change the information of a password");
             }
             String choice = input.nextLine();
 
-
             if(choice.equalsIgnoreCase("Add")){
                 System.out.println("Please enter the website the password relates to: ");
                 String web = input.nextLine();
-                System.out.println("Please enter the username the password relates to: ");
-                String user = input.nextLine();
-                System.out.println("Please enter the password: ");
-                String pass = input.nextLine();
-                Info insertInfo = new Info(web, user, pass);
-                passwords.add(insertInfo);
+                if(findWeb(passwords, web) != -1){
+                    System.out.println("ERROR: WEBSITE ALREADY ENTERED, USE CHANGE FUNCTION");
+                }else{
+                    System.out.println("Please enter the username the password relates to: ");
+                    String user = input.nextLine();
+                    System.out.println("Please enter the password: ");
+                    String pass = input.nextLine();
+                    Info insertInfo = new Info(web, user, pass);
+                    passwords.add(insertInfo);
+                }
                 
             }else if(choice.equalsIgnoreCase("Find")){
                 
@@ -121,6 +169,9 @@ public class passwordManager{
                 String findInfo = input.nextLine();
                 int position = findWeb(passwords, findInfo);
                 passwords.remove(position);
+
+            }else if(choice.equalsIgnoreCase("Quit")){
+                System.exit(0);
             
             }else{
                 System.out.println("Invalid Input!");
@@ -132,9 +183,19 @@ public class passwordManager{
                 flag = 1;
             }else{
                 flag = 0;
+                writer.write(masterPass + "\n");
+                for(int i = 0; i < passwords.size(); i++){
+                    writer.write(passwords.get(i).getWeb() + "!");
+                    writer.write(passwords.get(i).getUser() + "@");
+                    writer.write(passwords.get(i).getPass());
+                    writer.write("\n");
+
+                }
+                
             }
         }while(flag == 1);
         
+        writer.close();
         input.close();
     }
 }
